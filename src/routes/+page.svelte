bc75f1cf726125c811b39f247da9103a432fd8a4
Gaurav Ramesh <gramesh@opentable.com>
2025-01-18 20:26:09 -0800

    adds skeleton ui to style the site; adds basic styling from it

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,11 +2,16 @@
 	// SETUP: Add this to use Tailwind in your Svelte components
 	import '../app.css';
 	export let data;
-	
+
+	import { Avatar } from '@skeletonlabs/skeleton';
+  import { RadioGroup, RadioItem } from '@skeletonlabs/skeleton';
+  import 'iconify-icon';
+
 	let showModal = false;
 	let selectedArticle = null;
 	let articleContent = '';
 	let isLoading = false;
+  let value = 0;
 
 	async function handleViewClick(url) {
 		alert("viewing article", url);
@@ -42,82 +47,108 @@
 	}
 </script>
 
-<table class="min-w-full divide-y divide-gray-200">
-	<thead class="bg-gray-50">
-		<tr>
-			<th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-				Title
-			</th>
-			<th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-				Date Published
-			</th>
-			<th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-				Author
-			</th>
-			<th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-				Site
-			</th>
-			<th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-				Length
-			</th>
-			<th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-				Reading Time
-			</th>
-			<th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-				Tags
-			</th>
-			<th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-				Actions
-			</th>
-		</tr>
-	</thead>
-	<tbody class="bg-white divide-y divide-gray-200">
-		{#each data.bookmarks.slice(0, 20) as bookmark (bookmark.id)}
-			<tr>
-				<td class="px-6 py-4 whitespace-nowrap">
-					<div class="text-sm text-gray-900">
-						<a href={bookmark.url} target="_blank">{bookmark.title}</a>
-					</div>
-				</td>
-				<td class="px-6 py-4 whitespace-nowrap">
-					<div class="text-sm text-gray-900">{bookmark.date}</div>
-				</td>
-				<td class="px-6 py-4 whitespace-nowrap">
-					<div class="text-sm text-gray-900">{bookmark.author}</div>
-				</td>
-				<td class="px-6 py-4 whitespace-nowrap">
-					<div class="text-sm text-gray-900">{bookmark.site}</div>
-				</td>
-				<td class="px-6 py-4 whitespace-nowrap">
-					<div class="text-sm text-gray-900">{bookmark.length}</div>
-				</td>
-				<td class="px-6 py-4 whitespace-nowrap">
-					<div class="text-sm text-gray-900">{bookmark.reading_time}</div>
-				</td>
-				<td class="px-6 py-4 whitespace-nowrap">
-					<div class="text-sm text-gray-900">
-						{#each bookmark.tags as tag}
-							<span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">{tag}</span>
-						{/each}
-					</div>
-				</td>
-				<td class="px-6 py-4 whitespace-nowrap">
-					<div class="text-sm text-gray-900">
-						<button class="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded" on:click={() => handleViewClick(bookmark.url)}>
-							View
-						</button>
-						<button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-							Edit
-						</button>
-						<button class="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded">
-							Delete
-						</button>
-					</div>
-				</td>
-			</tr>
-		{/each}
-	</tbody>
-</table>
+<div class="container mx-auto p-8 space-y-8">
+  <div class="flex justify-start items-end">
+    <h1 class="h1 mr-5">context.app</h1>
+    <p class="text-lg text-gray-500">| Your context on the web</p>
+  </div>
+	
+  <section class="flex justify-end items-center">
+		<!-- <a class="btn variant-soft-primary" href="https://kit.svelte.dev/">SvelteKit</a>
+		<a class="btn variant-soft-secondary" href="https://tailwindcss.com/">Tailwind</a>
+		<a class="btn variant-soft-tertiary" href="https://github.com/">GitHub</a> -->
+
+
+    <RadioGroup>
+      <RadioItem bind:group={value} name="justify" value={0}>
+        <!-- <iconify-icon icon="material-symbols:table-outline-sharp" width="24" height="24"></iconify-icon> -->Table
+      </RadioItem>
+      <RadioItem bind:group={value} name="justify" value={1}>
+        <!-- <iconify-icon icon="material-symbols:table-rows-narrow-rounded" width="24" height="24"></iconify-icon> --> List
+      </RadioItem>
+      <RadioItem bind:group={value} name="justify" value={2}>
+        <!-- <iconify-icon icon="material-symbols:table-outline-sharp" width="24" height="24"></iconify-icon> --> Gallery
+      </RadioItem>
+    </RadioGroup>
+
+	</section>
+
+  <div class="table-container">
+    <table class="table table-hover">
+      <thead class="bg-gray-50">
+        <tr>
+          <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+            Title
+          </th>
+          <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+            Date Published
+          </th>
+          <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+            Author
+          </th>
+          <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+            Site
+          </th>
+          <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+            Length
+          </th>
+          <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+            Reading Time
+          </th>
+          <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+            Tags
+          </th>
+          <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+            Actions
+          </th>
+        </tr>
+      </thead>
+      <tbody class="bg-white divide-y divide-gray-200">
+        {#each data.bookmarks.slice(0, 20) as bookmark (bookmark.id)}
+          <tr>
+            <td class="px-6 py-4 whitespace-nowrap">
+              <div class="text-sm text-gray-900">
+                <a href={bookmark.url} target="_blank">{bookmark.title}</a>
+              </div>
+            </td>
+            <td class="px-6 py-4 whitespace-nowrap">
+              <div class="text-sm text-gray-900">{bookmark.date}</div>
+            </td>
+            <td class="px-6 py-4 whitespace-nowrap">
+              <div class="text-sm text-gray-900">{bookmark.author}</div>
+            </td>
+            <td class="px-6 py-4 whitespace-nowrap">
+              <div class="text-sm text-gray-900">{bookmark.site}</div>
+            </td>
+            <td class="px-6 py-4 whitespace-nowrap">
+              <div class="text-sm text-gray-900">{bookmark.length}</div>
+            </td>
+            <td class="px-6 py-4 whitespace-nowrap">
+              <div class="text-sm text-gray-900">{bookmark.reading_time}</div>
+            </td>
+            <td class="px-6 py-4 whitespace-nowrap">
+              <div class="text-sm text-gray-900">
+                {#each bookmark.tags as tag}
+                  <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">{tag}</span>
+                {/each}
+              </div>
+            </td>
+            <td class="px-6 py-4 whitespace-nowrap">            
+              <div class="btn-group variant-filled">
+                <button>View</button>
+                <button>Edit</button>
+                <button>Delete</button>
+              </div>
+            </td>
+          </tr>
+        {/each}
+      </tbody>
+    </table>
+    
+  </div>
+</div>
+
+<Avatar src="https://i.pravatar.cc/" />
 
 {#if showModal}
 	<div class="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center">
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,29 @@
+import { join } from 'path';
+import { skeleton } from '@skeletonlabs/tw-plugin';
+
 /** @type {import('tailwindcss').Config} */
 export default {
+  // SETUP Skeleton UI
+  darkMode: 'class',
   // SETUP: Add all the files you want to style
-  content: ['./src/**/*.{html,js,svelte,ts}'],
+  content: [
+      './src/**/*.{html,js,svelte,ts}',
+      // Append the path to the skeleton labs files
+      join(require.resolve('@skeletonlabs/skeleton'), '../**/*.{html,js,svelte,ts}')
+    ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        serif: ['Libre Baskerville', 'serif'],
+				handwriting: ['Shantell Sans', 'sans-serif'],
+				sans: ['Fira Sans', 'sans-serif'],
+      },
+    },
   },
-  plugins: [],
+  plugins: [
+    skeleton({
+      themes: { preset: ['skeleton'] },
+    })
+  ],
 }
 
